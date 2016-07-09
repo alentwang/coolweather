a0d3c1f4de941df1087d116969c565c5323ff102
alentwang <1025657770@qq.com>
2016-07-09 16:41:36 +0800

    first commit

---
+++ app/src/main/java/com/alent/coolweather/MainActivity.java
@@ -0,0 +1,18 @@
+package com.alent.coolweather;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.litepal.tablemanager.Connector;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SQLiteDatabase db = Connector.getDatabase();
+    }
+
+}
---
+++ app/src/main/java/com/alent/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.alent.coolweather.model;
+
+/**
+ * Created by wyl on 2016/7/9.
+ */
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String ProvinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return ProvinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        ProvinceCode = provinceCode;
+    }
+}
